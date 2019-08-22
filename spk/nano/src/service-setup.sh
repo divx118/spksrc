daa05926ccff160866233541a7e0b477ad6a82b2
divx118 <mauricevankruchten@gmail.com>
2019-08-22 21:00:12 +0200

    Updated nano to version 4.3 and included code highlighting. Changed maintainer for nano and iperf.

--- spk/nano/src/service-setup.sh
+++ spk/nano/src/service-setup.sh
@@ -1,13 +1,14 @@
-
 service_postinst ()
 {
     # Put nano in the PATH
     mkdir -p /usr/local/bin
-    ln -s /var/packages/${SYNOPKG_PKGNAME}/target/bin/nano /usr/local/bin/nano
+    ln -sf /var/packages/${SYNOPKG_PKGNAME}/target/bin/nano /usr/local/bin/nano
+    echo 'include /var/packages/nano/target/share/nano/*.nanorc' > /root/.nanorc
 }
 
 service_postuninst ()
 {
     # Remove link
     rm -f /usr/local/bin/nano
+    rm -f /root/.nanorc
 }
